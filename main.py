c212ce8743a783cb31723d85daba9eae70cbf9cf
Hamza-theGOAT <hamza.b0302@gmail.com>
2025-07-29 23:07:13 +0500

    Updated script to have dynamic input path

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 import random
+import os
+from dotenv import load_dotenv
 
 
 def randomizeLetters(s):
+    # Set of letters and digits
     letters = [c for c in s if c.isalpha()]
     digits = [c for c in s if c.isdigit()]
     random.shuffle(letters)
@@ -22,21 +25,28 @@ def randomizeLetters(s):
 
 
 def main():
+    # Get environment variables
+    load_dotenv()
+    fileInput = os.getenv('fileInput')
+    shName = os.getenv('shName')
+    excluded = os.getenv('excluded').split(',')
+
+    # Get input file
     df = pd.read_excel(
-        'fileInput.xlsx',
-        sheet_name='Sheet1'
+        fileInput,
+        sheet_name=shName
     )
     print(f'Origina DataFrame:\n{df}')
     print('_'*100)
 
-    excluded = ['Name', 'Status']
-
+    # Shuffle data within input file cols
     for col in df.columns:
         if col not in excluded:
             df[col] = df[col].replace(np.nan, '')
             df[col] = df[col].astype(str).apply(randomizeLetters)
-
     print(f'Shuffled DataFrame:\n{df}')
+
+    # Save to excel
     df.to_excel('fileOutput.xlsx', sheet_name='Sheet1', index=False)
 
 
